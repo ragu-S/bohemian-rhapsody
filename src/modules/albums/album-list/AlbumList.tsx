c87ca168a704a74e5e740a88857b380c13bd1dcc
Ragu Sivananadha <ragu.sivanandha@gmail.com>
2025-01-14 21:55:43 -0500

    chore: fix type error for AlbumList

--- src/modules/albums/album-list/AlbumList.tsx
+++ src/modules/albums/album-list/AlbumList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AlbumTile from "../album-tile/AlbumTile";
 import { TAlbum } from "./types";
 
-const AlbumList = ({ albums }: { albums: TAlbum[] }) => {
+const AlbumList = ({ albums }: { albums: TAlbum[] | undefined  }) => {
   return <ul className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
     {
       albums?.map(({ id, ...album }) => <AlbumTile id={id} key={id} {...album} />)
